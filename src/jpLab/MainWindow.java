9b79a12da41e3e7449b28934f0e5f387dd1cc20a
Krupahombre <kacper.krupinski01@gmail.com>
2022-01-19 15:02:01 +0100

    Few modifications of code in MainWindow

--- src/jpLab/MainWindow.java
+++ src/jpLab/MainWindow.java
@@ -4,27 +4,29 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainWindow extends JFrame {
-    int windowWidth;
-    int windowHeight;
-    JPanel jPanel;
+    private final int WINDOW_WIDTH = 1000;
+    private final int WINDOW_HEIGHT = 800;
+    private JPanel jPanel;
 
     public MainWindow() {
         this.setTitle("agar.io");
-        windowWidth = 640;
-        windowHeight = 480;
-        this.setSize(windowWidth, windowHeight);
-        int width = Toolkit.getDefaultToolkit().getScreenSize().width;
-        int height = Toolkit.getDefaultToolkit().getScreenSize().height;
-        int screenWidth = this.getSize().width;
-        int screenHeight = this.getSize().height;
-        this.setLocation((width - screenWidth) / 2, (height - screenHeight) / 2);
+        this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        this.setLocationRelativeTo(null);
+        /**
+         * Zamieniłem te 5 linijek w jedną (16 linia)
+         */
+//        int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+//        int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+//        int screenWidth = this.getSize().width;
+//        int screenHeight = this.getSize().height;
+//        this.setLocation((width - screenWidth) / 2, (height - screenHeight) / 2);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         initComponents();
     }
 
     private void initComponents() {
         jPanel = new JPanel();
-        jPanel.setBounds(0, 0, windowWidth, windowHeight);
+        jPanel.setBounds(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
         setContentPane(jPanel);
         jPanel.setLayout(null);
     }
